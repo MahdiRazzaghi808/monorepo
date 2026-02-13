ae2e4e10d0e842bec9e63e445db9b06465278c88
MahdiRazzaghi808 <razzaghi.mahdi80@gmail.com>
2026-02-13 12:16:14 +0330

    signin initial ui

--- apps/admin/app/page.tsx
+++ apps/admin/app/page.tsx
@@ -1,8 +1,13 @@
+"use client"
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter()
+  router.push('signin')
+
   return (
     <div>
-
+      چیزی برای نمایش نیست...
     </div>
   );
 }
